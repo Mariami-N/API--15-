74f8131dde23b22c6cf242cab2b7cea2331f042b
Mariami-N <avetianimari@gmail.com>
2023-02-26 09:37:28 -0800

    api cont

---
+++ main.js
@@ -0,0 +1,6 @@
+let container=document.querySelector(".container");
+let box=document.querySelector(".box");
+let advicebox=document.querySelector(".advicebox");
+let btn=document.querySelector(".btn");
+let h5=document.querySelector("h5");
+ let paragraph=document.querySelector("p");
