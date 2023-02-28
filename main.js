460f4dbfd6c5ad987591d050b00cd6a8c63a53ce
Mariami-N <avetianimari@gmail.com>
2023-02-28 01:22:49 -0800

    api api api

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ let h5=document.querySelector("h5");
 let paragraph=document.querySelector("p");
 
 
-
+let randomNum = Math.floor(Math.random() * 300);
+    axios.get(`https://api.adviceslip.com/advice?random=${randomNum}`).then(response => {
+        let advice = response.data.slip.advice;
+        paragraph.innerHTML = `"${advice}"`;
+        let id = response.data.slip.id;
+            h5.innerHTML=`ADVICE #${id}`;
+      })
     btn.addEventListener("click", () => {
     let randomNum = Math.floor(Math.random() * 300);
     axios.get(`https://api.adviceslip.com/advice?random=${randomNum}`).then(response => {
